d4619192ad5af6635be0a178f60cf055bee5cd59
Finn Ellis <relsqui@chiliahedron.com>
2016-05-24 23:54:03 -0700

    Switch maketxt to unix mode.

--- maketxt.sh
+++ maketxt.sh
@@ -4,6 +4,8 @@ for set in *.json; do
     echo "$set ..." >&2
     # unix2dos converts line endings for the sake of Windows-based concordancers
     # it's in the dos2unix package (named for the opposite tool)
-    cat $set | ./mtgcorpus.py | unix2dos > $(basename $set .json).txt
+    # if you need that, uncomment the following line and comment the one after
+    # cat $set | ./mtgcorpus.py | unix2dos > $(basename $set .json).txt
+    cat $set | ./mtgcorpus.py > $(basename $set .json).txt
 done
 echo "done" >&2
