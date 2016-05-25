664648f8a117eb8300855d9fa390a40d3468f031
Finn Ellis <finnre@pdx.edu>
2016-05-25 01:55:25 -0700

    Pull parsey bits into conllize instead of relying on external demo.

--- conllize.sh
+++ conllize.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
 TESTING=false
+PARSER_EVAL=bazel-bin/syntaxnet/parser_eval
+MODEL_DIR=syntaxnet/models/parsey_mcparseface
+INPUT_FORMAT=stdin
 
 fake_process() {
     echo "v-- I WOULD PROCESS THIS PART --v"
     echo -e "$1"
     echo "^-------------------------------^"
+}
+
+parsey() {
+    $PARSER_EVAL \
+      --input=$INPUT_FORMAT \
+      --output=stdout-conll \
+      --hidden_layer_sizes=64 \
+      --arg_prefix=brain_tagger \
+      --graph_builder=structured \
+      --task_context=$MODEL_DIR/context.pbtxt \
+      --model_path=$MODEL_DIR/tagger-params \
+      --slim_model \
+      --batch_size=1024 \
+      --alsologtostderr \
+       | \
+      $PARSER_EVAL \
+      --input=stdin-conll \
+      --output=stdout-conll \
+      --hidden_layer_sizes=512,512 \
+      --arg_prefix=brain_parser \
+      --graph_builder=structured \
+      --task_context=$MODEL_DIR/context.pbtxt \
+      --model_path=$MODEL_DIR/parser-params \
+      --slim_model \
+      --batch_size=1024 \
+      --alsologtostderr
 }
 
 cd ~/models/syntaxnet
@@ -30,7 +59,7 @@ while read line; do
             if $TESTING; then
                 fake_process "$card"
             else
-                echo "$card" | syntaxnet/demo.sh 2>/dev/null
+                echo "$card" | parsey
             fi
             card=""
         fi
